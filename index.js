eb100e07c76bc18fb562472973b02c8cc6d660ab
Omer Shamai <renginald.bagel@gmail.com>
2018-10-08 10:05:49 +0300

    more stuff

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let express = require('express');
 let app = express();
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey('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')
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/" + "index.html");
@@ -11,6 +14,19 @@ app.get('/main.js', (req, res) => {
 
 app.get('/profile', (req, res) => {
    res.sendFile(__dirname + "/" + "profile.html");
+});
+
+const msg = {
+    to: 'renginald.bagel@gmail.com',
+    from: 'renginald.bagel@gmail.com',
+    subject: 'test',
+    text: 'test',
+    html: '<strong>test</strong>'
+};
+
+app.get('/mail', (req, res) => {
+    res.sendFile(__dirname + "/" + "mail.html");
+    sgMail.send(msg);
 });
 
 app.listen(8080, () => {
